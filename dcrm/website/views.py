1c00960566a62fc930adb89ff824179ea8a7871e
Serge Demanou <demanou05@yahoo.fr>
2024-10-24 18:25:16 +0000

    second commit

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import *
 
 
 # Create your views here.
@@ -28,3 +29,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged Out.')
     return redirect('home')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}. You are now able to log in.')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
